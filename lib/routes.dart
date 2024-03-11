fa6c56111c35a4bd0a254b648ea882fc1a641440
simranDung30 <141987334+simranDung30@users.noreply.github.com>
2024-03-11 22:29:28 +0530

    Update routes.dart

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,10 +1,12 @@
 import 'package:brain_school/screens/login_screen/login_screen.dart';
+import 'package:brain_school/screens/quiz_screen/quiz_screen.dart';
 import 'package:brain_school/screens/splash_screen/splash_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'screens/assignment_screen/assignment_screen.dart';
 import 'screens/datesheet_screen/datesheet_screen.dart';
 import 'screens/fee_screen/fee_screen.dart';
 import 'screens/home_screen/home_screen.dart';
+import 'screens/quiz_screen/quiz_screen.dart';
 import 'screens/my_profile/my_profile.dart';
 
 Map<String, WidgetBuilder> routes = {
@@ -14,6 +16,7 @@ Map<String, WidgetBuilder> routes = {
   HomeScreen.routeName: (context) => HomeScreen(),
   MyProfileScreen.routeName: (context) => MyProfileScreen(),
   FeeScreen.routeName: (context) => FeeScreen(),
+  QuizScreen.routeName: (context) => QuizScreen(),
   AssignmentScreen.routeName: (context) => AssignmentScreen(),
   DateSheetScreen.routeName: (context) => DateSheetScreen(),
 };
